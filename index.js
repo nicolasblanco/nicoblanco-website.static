b5f4f502733b5e9b4b93051555b02849fee7532f
Nicolas Blanco <nicolas@nicolasblanco.fr>
2017-02-06 23:23:06 +0100

    Grooming...

--- index.js
+++ index.js
@@ -3,24 +3,34 @@ var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
 var permalinks  = require('metalsmith-permalinks');
 var sass        = require('metalsmith-sass');
+var collections = require('metalsmith-collections');
 var s3          = require('metalsmith-s3');
 var msIf        = require('metalsmith-if');
 var partials    = require('metalsmith-discover-partials');
 var redirect    = require('metalsmith-redirect');
 var serve       = require('metalsmith-serve');
+var formatcheck = require('metalsmith-formatcheck');
+var debug       = require('metalsmith-debug');
 
 const latestArticleSlug = 'beautify-your-rails-confirm-boxes-in-a-few-seconds';
 
 Metalsmith(__dirname)
   .metadata({
-    title: "My Static Site & Blog",
-    description: "It's about saying »Hello« to the World.",
+    title: "Nicolas Blanco",
+    description: "The home of a passionate Web architect",
     generator: "Metalsmith",
-    url: "http://www.metalsmith.io/"
+    url: "http://nicolasblan.co"
   })
   .source('./src')
   .destination('./build')
   .clean(true)
+  .use(collections({
+    articles: {
+      pattern: 'blog/*.html',
+      sortBy: 'date',
+      reverse: true
+    }
+  }))
   .use(markdown())
   .use(permalinks())
   .use(partials())
@@ -33,6 +43,7 @@ Metalsmith(__dirname)
   .use(redirect({
     '/blog': `/blog/${latestArticleSlug}`
   }))
+  //.use(formatcheck({ verbose: true }))
   .use(msIf(
     process.env.NODE_ENV != 'production', serve()
   ))
